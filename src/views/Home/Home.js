53b6ca5664a3fb9ff2f229cdd966952331410179
Cole Rossman <colerossman99@gmail.com>
2022-03-22 16:48:02 -0700

    Home.js added return info for home page and PageNavigation implements NavLink to navigate pages

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <h1>Welcome to my page of random information!</h1>
+      <h3>This app utilizes React Routers and data tables in Supabase.</h3>
+    </>
   );
 }
