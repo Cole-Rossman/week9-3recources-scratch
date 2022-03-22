da1fcfab321ae79f9308327c20e6372ea0613f44
Cole Rossman <colerossman99@gmail.com>
2022-03-22 16:05:12 -0700

    bunnies.js fetches bunnies and Bunnies.js implements use effect then returns mapped bunny names

--- src/views/Bunnies/Bunnies.js
+++ src/views/Bunnies/Bunnies.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchBunnies } from '../../services/bunnies';
 
 export default function Bunnies() {
+  const [bunnies, setBunnies] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const bunnyData = await fetchBunnies();
+        setBunnies(bunnyData);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Bunnies</div>
+    <div className='bunnies'>
+      <h1>Fuzzy Bunny Friends</h1>
+      {error && <p>{error}</p>}
+      {bunnies.map((bunny) => (
+        <p key={bunny.id}>{bunny.name}</p>
+      ))}
+    </div>
   );
 }
